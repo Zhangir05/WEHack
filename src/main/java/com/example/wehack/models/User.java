a8814add0fd709e643e6f6511f27532fe39ddaea
Nurgissa Sagynbek <nurgisasaginbek@gmail.com>
2024-05-05 14:11:42 +0500

    Nurgisa

--- src/main/java/com/example/wehack/models/User.java
+++ src/main/java/com/example/wehack/models/User.java
@@ -1,13 +1,40 @@
 package com.example.wehack.models;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.Date;
+
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
+@Entity
+@Table(name = "users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String name;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "father_name")
+    private String fatherName;
+
+    @Column(name = "birth_date")
+    private Date birthDate;
+
+    @Column(name = "status")
+    private String status;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
+    private String password;
 }
